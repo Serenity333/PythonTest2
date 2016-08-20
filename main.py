146dad6395405af9407a6aabfb774be73f003b36
Amanda McCumber <Serenity333@users.noreply.github.com>
2016-08-19 22:37:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import *
 root = Tk()
 
 pic = Text(root, height = 30, width = 30)
-photo = PhotoImage(file='./photos/bronze_dragon.gif')
+photo = PhotoImage(file='./photos/dragon_bronze.gif')
 pic.insert(END,'\n')
 pic.image_create(END, image=photo)
 
