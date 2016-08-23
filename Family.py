9fa240032f5d2a617533c5edd0a63fe7b34e87fa
Amanda McCumber <Serenity333@users.noreply.github.com>
2016-08-23 18:51:09 -0400

    Update Family.py

--- Family.py
+++ Family.py
@@ -1,6 +1,16 @@
 from tkinter import *
 
-root = Tk()
+class App(tk.Frame):
+  def __init__(self, master=NONE):
+    super().__init__(master)
+    self.pack()
+
+#create application
+myapp = Tk()
+
+#method calls to the window manager class
+myapp.master.title("Family")
+myapp.master.maxsize(1000,400)
 
 pic = Text(root, height = 30, width = 30)
 photo1 = PhotoImage(file="dragon_bronze.gif")
@@ -8,8 +18,8 @@ photo1.image = photo
 pic.insert(END,'\n')
 pic.image_create(END, image=photo)
 
-pic.pack(side=LEFT)
-photo1.pack(side=LEFT)
+pic.pack(side="left")
+photo1.pack(side="left")
 
 cat1 = Text(root, height=30, width = 30)
 scroll = Scrollbar(root, command=cat1.yview)
@@ -35,4 +45,5 @@ cat1.insert(END, 'follow-up\n', 'follow')
 cat1.pack(side=LEFT)
 scroll.pack(side=RIGHT, fill=Y)
 
-root.mainloop()
+
+myapp.mainloop()
